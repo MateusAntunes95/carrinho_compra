7712b5021cc52bdd4b2ad5bf3d4cea7872d19a59
Mateus Antunes <mateus.antunes95@gmail.com>
2023-07-01 17:37:47 -0300

    arruma form de discount e user

--- resources/views/discount_doupon/_form.blade.php
+++ resources/views/discount_doupon/_form.blade.php
@@ -3,46 +3,54 @@
         <h5 class="mb-0">Editando cupom</h5>
     </div>
     <div class="card-body">
-        <div class="form-group">
-            <div class="col-sm-3 mb-4">
-                <label for="name">Nome:</label>
-                <input name="name" id="name" class="form-control rounded" required value="{{ $doupon->name }}">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="name">Nome:</label>
+                    <input name="name" id="name" class="form-control rounded" required
+                        value="{{ $doupon->name }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-3 mb-3">
-                <label for="locator">Localizador:</label>
-                <input name="locator" class="form-control rounded" required value="{{  $doupon->locator }}">
+            <div class="col-sm-3">
+                <div class="form-group">
+                    <label for="locator">Localizador:</label>
+                    <input name="locator" class="form-control rounded" required value="{{ $doupon->locator }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
             <div class="col-sm-3 mb-3">
-                <label for="discount">Desconto:</label>
-                <input type="number" name="discount" class="form-control rounded" required value="{{  $doupon->discount }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-3 mb-3">
-                <label for="limit">Limite:</label>
-                <input type="number" name="limit" class="form-control rounded" required value="{{  $doupon->limit }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-3 mb-3">
-                <label for="expiration_date">Data de expiração:</label>
-                <input type="date" name="expiration_date" class="form-control rounded" required value="{{  $doupon->expiration_date }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-3 mb-3">
-                <label for="active" class="form-check-label">Ativo?</label>
-                <div class="form-check">
-                    <input type="checkbox" checked name="active" id="active" class="form-check-input" {{ $doupon->active ? 'checked' : '' }}>
-                    <label class="form-check-label" for="active"></label>
+                <div class="form-group">
+                    <label for="discount">Desconto:</label>
+                    <input type="number" name="discount" class="form-control rounded" required
+                        value="{{ $doupon->discount }}">
                 </div>
             </div>
         </div>
-
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="form-group">
+                    <label for="limit">Limite:</label>
+                    <input type="number" name="limit" class="form-control rounded" required
+                        value="{{ $doupon->limit }}">
+                </div>
+            </div>
+            <div class="col-sm-3 mb-3">
+                <div class="form-group">
+                    <label for="expiration_date">Data de expiração:</label>
+                    <input type="date" name="expiration_date" class="form-control rounded" required
+                        value="{{ $doupon->expiration_date }}">
+                </div>
+            </div>
+            <div class="col-sm-3 mb-3">
+                <div class="form-group">
+                    <label for="active" class="form-check-label">Ativo?</label>
+                    <div class="form-check">
+                        <input type="checkbox" checked name="active" id="active" class="form-check-input"
+                            {{ $doupon->active ? 'checked' : '' }}>
+                        <label class="form-check-label" for="active"></label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="card-footer">
         <button type="submit" class="btn btn-success">
--- resources/views/user/_form.blade.php
+++ resources/views/user/_form.blade.php
@@ -3,28 +3,31 @@
         <h5 class="mb-0">Editando usuário</h5>
     </div>
     <div class="card-body">
-        <div class="form-group">
+        <div class="row">
             <div class="col-sm-3 mb-4">
-                <label for="name">Nome:</label>
-                <input name="name" id="name" class="form-control rounded" required value="{{ $user->name }}">
+                <div class="form-group">
+                    <label for="name">Nome:</label>
+                    <input name="name" id="name" class="form-control rounded" required
+                        value="{{ $user->name }}">
+                </div>
             </div>
+            <div class="col-sm-4 mb-3">
+                <div class="form-group">
+                    <label for="email">email:</label>
+                    <input name="email" class="form-control rounded" required value="{{ $user->email }}">
+                </div>
+            </div>
+            @if (empty($user->id))
+                <div class="col-sm-4 mb-3">
+                    <div class="form-group">
+                        <label for="password">Senha</label>
+                        <input name="password" class="form-control rounded" required>
+                    </div>
+            @endif
         </div>
-        <div class="form-group">
-            <div class="col-sm-3 mb-3">
-                <label for="email">email:</label>
-                <input name="email" class="form-control rounded" required value="{{  $user->email }}">
-            </div>
+        <div class="card-footer">
+            <button type="submit" class="btn btn-success">
+                Salvar
+            </button>
         </div>
-        @if(empty($user->id))
-            <div class="form-group">
-                <label for="password">Senha</label>
-                <input name="password" class="form-control rounded" required >
-            </div>
-        @endif
     </div>
-    <div class="card-footer">
-        <button type="submit" class="btn btn-success">
-            Salvar
-        </button>
-    </div>
-</div>
